960eaff90901cee5a41f66937b0eef4980e2d78a
student <bukindimon444@gmail.com>
2023-05-23 20:23:35 +0300

    Друге завдання

--- praktychka_23_05_23.py
+++ praktychka_23_05_23.py
@@ -34,7 +34,22 @@ a.divide(8,4)
 '''
 #2
 
+import logging
 
+def write_file(fill_path , data):
+    try:
+        with open(fill_path , "w") as file:
+            file.write(data)
+    except Exception as e:
+        pass
+logging.basicConfig(level = logging.DEBUG ,
+                    filename = "info.txt" ,
+                    filemode = "w" ,
+                    format = '%(asctime)s - %(levelname)s - %(message)s')
+write_file("info.txt" , input("Введіть будь що і я виведу це в окремий файл:"))
+
+
+#3
 
 
 
