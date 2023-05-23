452dc039606fc1918a2f0e2b2ccb2597dd1f29c0
student <bukindimon444@gmail.com>
2023-05-23 19:01:21 +0300

    Винятки

--- class_work.py
+++ class_work.py
@@ -1,7 +1,16 @@
 import logging #стандартна бібліотека для логування перебігу програми
-logging.basicConfig(level = logging.DEBUG , format = '%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level = logging.DEBUG ,
+                    filename = "logs.log" ,
+                    filemode = "w" ,
+                    format = '%(asctime)s - %(levelname)s - %(message)s')
 logging.debug("debug") #показує стандартний перебіг програми
 logging.info("info")
 logging.error("error")
 logging.warning("warning")
 logging.critical("critical")
+
+try:
+    print(10/0)
+except Exception:
+    logging.exception("Fignya")
+
